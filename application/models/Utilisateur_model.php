47069e1733267ed40518fd17d270c391907b22de
ines-missoum <32544041+ines-missoum@users.noreply.github.com>
2018-06-03 16:42:43 +0200

    corrections

--- application/models/Utilisateur_model.php
+++ application/models/Utilisateur_model.php
@@ -59,28 +59,9 @@ class Utilisateur_model extends CI_Model{
 	    public function insert($data) {
 	        
 
-		 	/*$this->db->set('nom_utilisateur', $data['nom_utilisateur'])
-		 	->set('mot_de_passe', $data['mot_de_passe'])
-		 	->set('nom', $data['nom'])
-		 	->set('prenom', $data['prenom'])
-		 	->set('ville', $data['ville'])
-		 	->set('nb_points', $data['nb_points'])
-		 	->insert($this->table);*/
-
 			$this->db->insert('utilisateur', $data);
 
 		}
-
-		/*public function get_nom(){ 
-	    
-
-		 	$result = $this->db->select('prenom')
-		 						->from($this->table)	
-		 						->where('nom_utilisateur',get_cookie('cookieUtilisateur'))
-		 						->get()
-		 						->result();
-		 	 return $result;	
-    	}*/
 	
 
 
